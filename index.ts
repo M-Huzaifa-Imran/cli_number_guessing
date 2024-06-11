62eb09ddcf3df93a0112cfbef606091d1e3850dc
M Huzaifa Imran <mhuzaifaimran106@gmail.com>
2024-06-11 16:36:31 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+
+const randomNum = Math.floor(Math.random() * 6) ;
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGussedNumber",
+        type: "number",
+        message:"I rolled a dice guess the Number:",
+    },
+]);
+console.log(answer);
+
+if (answer.userGussedNumber === randomNum){
+    console.log("Congratulations! You guess the right number"); 
+}
+else {
+    console.log("You guess the wrong number");
+
+}
+console.log("The number is" , randomNum)
